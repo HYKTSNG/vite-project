0d7ef8cc45b98ab7fb2fbe49aa593a44a98bffed
HYKTSNG <Sin5hayak@gmail.com>
2023-06-12 22:49:54 +0900

    購入物名を可変に

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [donationLimit, setDonationLimit] = useState(0); // 寄付上限額を保持する状態変数
   const [result, setResult] = useState(0); // 合計値を保持する状態変数
 
-  const handleInputChange = (index, value) => {
+  const handleInputChange = (index: number, value: string) => {
     // 入力欄の値が変更されたときに呼び出される関数
     const newInputs = [...inputs];
     newInputs[index] = value;
@@ -18,7 +18,7 @@ function App() {
     setInputs([...inputs, ""]);
   };
 
-  const handleDonationLimitChange = (event) => {
+  const handleDonationLimitChange = (event: { target: { value: any } }) => {
     // 寄付上限額の入力欄の値が変更されたときに呼び出される関数
     setDonationLimit(Number(event.target.value));
   };
@@ -33,7 +33,7 @@ function App() {
     <>
       <h1>ふるさと納税いくらできるのかな--</h1>
       <h2>寄付上限額: {donationLimit}円</h2>
-
+      寄付上限額入力してね：
       <input
         type="number"
         value={donationLimit}
@@ -43,6 +43,7 @@ function App() {
         {inputs.map((input, index) => (
           <div key={index}>
             <label htmlFor={`input${index}`}>購入物 {index + 1}:</label>
+            <input type="text" id={`input${index}`} defaultValue="購入物" />
             <input
               type="number"
               id={`input${index}`}
